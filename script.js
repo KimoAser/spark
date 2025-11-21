e7d66e284e872888a9b840ef0b787f35e8244d08
Hakeem Adel <hakeemaseradel@gmail.com>
2025-11-21 09:56:38 +0300

    files added

---
+++ script.js
@@ -0,0 +1,112 @@
+const heroHeadings = document.querySelector(".hero_headings")
+const scroller = document.querySelectorAll(".clients_scroller")
+const upperFeatures = document.querySelectorAll(".upper_feature")
+const lowerFeatures = document.querySelectorAll(".lower_feature")
+let valueDisplays = document.querySelectorAll(".num")
+const firstPin = document.querySelector(".pinned_container_first")
+const secondPin = document.querySelector(".pinned_container_second")
+const thirdPin = document.querySelector(".pinned_container_third")
+const fourthPin = document.querySelector(".pinned_container_fourth")
+
+
+/*--hero section--*/ 
+const observer = new IntersectionObserver((entries)=>{
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.add("show");
+    }
+},{})
+observer.observe(heroHeadings);
+
+
+/*--Slider Clients section--*/ 
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+function addAnimation(){
+    scroller.forEach((scrollers)=>{
+        scrollers.setAttribute("data-animated",true);
+        const scrollInner = scrollers.querySelector(".clients_scroller_inner");
+        const scrollerContent = Array.from(scrollInner.children);
+        scrollerContent.forEach(item =>{
+            const duplicatedItems = item.cloneNode(true);
+            duplicatedItems.setAttribute("aria-hidden",true);
+            scrollInner.appendChild(duplicatedItems)
+        })
+    })
+}
+
+
+/*--features section--*/
+const observe = new IntersectionObserver((entires=>{
+    entires.forEach(entry=>{
+        if (entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    })
+}),{
+    rootMargin:"0px 0px -150px 0px"
+})
+upperFeatures.forEach(div=>observe.observe(div));
+lowerFeatures.forEach(div=>observe.observe(div));
+
+
+/*--Counter section--*/ 
+let interval = 1000;
+let started = false;
+let observeCounter = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && !started) {
+            started = true; // lock so it runs only once
+
+            valueDisplays.forEach((valueDisplay) => {
+                let startValue = 0;
+                let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+                let duration = Math.floor(interval / endValue);
+
+                let counter = setInterval(() => {
+                    startValue += 1;
+                    valueDisplay.textContent = startValue;
+
+                    if (startValue === endValue) clearInterval(counter);
+                }, duration);
+            });
+        }
+    });
+}, {
+    rootMargin:"0px 0px -50px 0px"
+});
+observeCounter.observe(document.querySelector(".counters_container"));
+
+/*--choose us pins--*/
+const pinObserveFirst = new IntersectionObserver((entries)=>{
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.add("show");
+    }
+},{
+    rootMargin:"0px 0px -180px 0px"
+}) 
+const pinObserveSecond = new IntersectionObserver((entries)=>{
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.add("show");
+    }
+},{
+    rootMargin:"0px 0px -180px 0px"
+}) 
+const pinObserveThird = new IntersectionObserver((entries)=>{
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.add("show");
+    }
+},{
+    rootMargin:"0px 0px -50px 0px"
+}) 
+const pinObserveFourth = new IntersectionObserver((entries)=>{
+    if(entries[0].isIntersecting){
+        entries[0].target.classList.add("show");
+    }
+},{
+    rootMargin:"0px 0px -50px 0px"
+}) 
+pinObserveFirst.observe(firstPin);
+pinObserveSecond.observe(secondPin);
+pinObserveThird.observe(thirdPin);
+pinObserveFourth.observe(fourthPin);
